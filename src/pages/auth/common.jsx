cd71afd835121fb0a320a345d5a06b26038df3ff
Wafarm <qinyy07@gmail.com>
2024-01-31 10:47:06 +0800

    Refactor login page

---
+++ src/pages/auth/common.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Card } from "@/components/card/card.jsx";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+  user-select: none;
+`;
+
+const AuthCard = styled(Card)`
+  width: 30%;
+  min-width: 300px;
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const AuthBanner = styled.span`
+  font-family: "SL Logo", serif;
+  font-size: 3.125rem;
+  color: azure;
+`;
+
+export const AuthForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+export const AuthHint = styled.span`
+  color: gray;
+  text-align: center;
+`;
+
+export const AuthContainer = ({ children }) => {
+  return (
+    <Container>
+      <AuthBanner>StarLight</AuthBanner>
+      <AuthCard>{children}</AuthCard>
+    </Container>
+  );
+};
+
+AuthContainer.propTypes = {
+  children: PropTypes.element,
+};
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Card } from "@/components/card/card.jsx";
 import { InputGroup } from "@/components/form/input-group.jsx";
 import { VscAccount, VscLock } from "react-icons/vsc";
 import {
@@ -10,40 +9,7 @@ import { useState } from "react";
 import { rootDir } from "@/pages/auth/_routes.jsx";
 import path from "path-browserify";
 import { useNavigate } from "react-router-dom";
-
-const LoginContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 20px;
-  user-select: none;
-`;
-
-const LoginCard = styled(Card)`
-  width: 30%;
-  min-width: 300px;
-  max-width: 400px;
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-`;
-
-const LoginForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-`;
-
-const LoginBanner = styled.span`
-  font-family: "SL Logo", serif;
-  font-size: 3.125rem;
-  color: azure;
-`;
-
-const LoginHint = styled.span`
-  color: gray;
-  text-align: center;
-`;
+import { AuthContainer, AuthForm, AuthHint } from "@/pages/auth/common.jsx";
 
 const LoginOptionGroup = styled.div`
   display: flex;
@@ -98,49 +64,46 @@ export const LoginPage = () => {
   };
 
   return (
-    <LoginContainer>
-      <LoginBanner>StarLight</LoginBanner>
-      <LoginCard>
-        <LoginForm onSubmit={handleSubmit}>
-          <LoginHint>登录以管理您的皮肤</LoginHint>
-          <InputGroup
-            name="username"
-            value={inputs.username}
-            onChange={handleChange}
-            type="text"
-            placeholder="用户名"
-            icon={<VscAccount />}
-          />
-          <InputGroup
-            name="password"
-            value={inputs.password}
-            onChange={handleChange}
-            type="password"
-            placeholder="密码"
-            icon={<VscLock />}
-          />
-          <LoginOptionGroup>
-            <label>
-              <input
-                name="remember"
-                checked={inputs.remember}
-                type="checkbox"
-                onChange={handleCheckChange}
-              />
-              记住我
-            </label>
-            <a href={path.join(rootDir, "reset")}>
-              <span>忘记密码</span>
-            </a>
-          </LoginOptionGroup>
-          <ActionButtonGroup>
-            <ActionButtonPrimary>登录</ActionButtonPrimary>
-            <ActionButtonSecondary onClick={handleRegister}>
-              注册
-            </ActionButtonSecondary>
-          </ActionButtonGroup>
-        </LoginForm>
-      </LoginCard>
-    </LoginContainer>
+    <AuthContainer>
+      <AuthForm onSubmit={handleSubmit}>
+        <AuthHint>登录以管理您的皮肤</AuthHint>
+        <InputGroup
+          name="username"
+          value={inputs.username}
+          onChange={handleChange}
+          type="text"
+          placeholder="用户名"
+          icon={<VscAccount />}
+        />
+        <InputGroup
+          name="password"
+          value={inputs.password}
+          onChange={handleChange}
+          type="password"
+          placeholder="密码"
+          icon={<VscLock />}
+        />
+        <LoginOptionGroup>
+          <label>
+            <input
+              name="remember"
+              checked={inputs.remember}
+              type="checkbox"
+              onChange={handleCheckChange}
+            />
+            记住我
+          </label>
+          <a href={path.join(rootDir, "reset")}>
+            <span>忘记密码</span>
+          </a>
+        </LoginOptionGroup>
+        <ActionButtonGroup>
+          <ActionButtonPrimary>登录</ActionButtonPrimary>
+          <ActionButtonSecondary onClick={handleRegister}>
+            注册
+          </ActionButtonSecondary>
+        </ActionButtonGroup>
+      </AuthForm>
+    </AuthContainer>
   );
 };
