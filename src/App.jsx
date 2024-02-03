31a57bef1c985847ff0124a1ca9c7c71469d1b59
Wafarm <qinyy07@gmail.com>
2024-02-03 21:09:30 +0800

    Replace main.css with inline style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { RouterProvider } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
+import logoFont from "@/assets/font/Logo.ttf";
 import { router } from "@/pages/_routes";
 
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: SL Logo;
+        src: url(${logoFont});
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #root {
+        display: flex;
+        align-items: center;
+    }
+`;
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
